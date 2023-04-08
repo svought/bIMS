e3987f1a0058afab8e24e44dde2d82b17156138e
svought <superstv08@gmail.com>
2023-04-07 18:03:14 -0600

    CRUD Read method complete. Learned how to use TRUNCATE TABLE in MYSQL to delete all records from table and reset auto incremental values to 1. Also learned about ResultSet metadata and used this for column count in table instead of manually handling for each table in hashmap or switch statement. Building all CRUD operations with re-usability in mind. Formatting of results may need to be altered in the future.

--- bIMS.java
+++ bIMS.java
@@ -1,7 +1,9 @@
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 
 public class bIMS {
 
@@ -9,24 +11,13 @@ public class bIMS {
     private static final String user = "svought";
     private static final String password = "Svoughtpass00";
 
-    private static void connectToDB() {
-
-        // Change later to get user input and verify login credentials
-
-
-
-
-    }
-
     private static void Create(String tableName, String attributes, String contents) {
         // Code to create record in database
 
-        //Try to connect to database using username and password
+        // Try to connect to database using username and password
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Connecting...");
             Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected successfully");
 
             // Execute a select all statement as a test
             String sql = "INSERT INTO " + tableName + " " + attributes + "VALUES " + contents;
@@ -41,23 +32,38 @@ public class bIMS {
         }
     }
 
-    private static void Read() {
+    private static void Read(String tableName) {
         // Code to obtain information from record(s) from database
 
-        //Try to connect to database using username and password
+        // Try to connect to database using username and password
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Connecting...");
             Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected successfully");
 
             // Execute a select all statement as a test
-            String sql = "SELECT * FROM items";
+            String sql = "SELECT * FROM " + tableName;
+            ResultSet result = conn.createStatement().executeQuery(sql);
+            ResultSetMetaData metaData = result.getMetaData();
             Statement statement = conn.createStatement();
             statement.execute(sql);
 
+            // Print table header
+            for (int i = 1; i < metaData.getColumnCount(); i++) {
+                System.out.print(metaData.getColumnName(i) + "\t");
+            }
+            System.out.println("\n");
 
-            System.out.println();
+            // Print results
+            while(result.next()){
+                for (int i = 1; i < metaData.getColumnCount(); i++) {
+                    System.out.print("\t" + result.getString(i) + "\t\t");
+                }
+                System.out.println();
+            }
+
+            // Close result set and connection
+            result.close();
+            conn.close();
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             throw new RuntimeException(ex);
@@ -73,13 +79,14 @@ public class bIMS {
     }
 
     public static void bIMSDriver() {
-        //connectToDB();
+        // Create Manufacturer in DB
+        // Create("manufacturers", "(ManufacturerName, ManufacturerPhoneNumber, ManufacturerEmail)", "('testManufacturer', 4065956593, 'test@gmail.com')");
 
-        //Create Manufacturere in DB
-        Create("manufacturers", "(ManufacturerName, ManufacturerPhoneNumber, ManufacturerEmail)", "('testManufacturer', 4065956593, 'test@gmail.com')");
+        // Create item in DB
+        // Create("items", "(Name, Cost, SalePrice, Quantity, Location, ManufacturerID)", "('testItem', 2, 3, 10, '4E', 0)");
 
-        //Create item in DB
-        //Create("items", "(Name, Cost, SalePrice, Quantity, Location, ManufacturerID)", "('testItem', 2, 3, 10, '4E', 0)");
+        // Read manufacturers table
+        Read("manufacturers");
     }
 
    public static void main(String[] args) {
